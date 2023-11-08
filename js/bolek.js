62a09361d82add5a4a6b85966e9896736ed7658e
Dzmitry Kushniaruk <xdzmkus@gmail.com>
2023-11-09 00:56:58 +0300

    total frames in match

--- js/bolek.js
+++ js/bolek.js
@@ -21,10 +21,15 @@ function getCommonMatchInfo(splitedValues){
     player1Name: splitedValues[0],
     player2Name: splitedValues[1],
 
+    player1Hadicap: splitedValues[3],
+    player2Hadicap: splitedValues[4],
+
     player1MatchPoint: splitedValues[7],
     player2MatchPoint: splitedValues[8],
 
     player1Break: splitedValues[12] == 0 ? true : false,
+
+    matchTotalFrames: splitedValues[14],
 
     player1FramePoint: splitedValues[15],
     player2FramePoint: splitedValues[16]
@@ -55,15 +60,37 @@ xjs.ready().then(function() {
       var splitedValues = message.toString().split('~');
       var commonMatchInfo = getCommonMatchInfo(splitedValues);
 
-      document.getElementById('ply1-name').innerHTML = commonMatchInfo.player1Name;
-      document.getElementById('ply2-name').innerHTML = commonMatchInfo.player2Name;
+      // fill players name and handicap
+      if(commonMatchInfo.player1Hadicap == '' && commonMatchInfo.player2Hadicap == '')
+      {
+        document.getElementById('ply1-name').innerHTML = commonMatchInfo.player1Name;
+        document.getElementById('ply2-name').innerHTML = commonMatchInfo.player2Name;
+      }
+      else
+      {
+        document.getElementById('ply1-name').innerHTML = commonMatchInfo.player1Name + " (" + commonMatchInfo.player1Hadicap + ")";
+        document.getElementById('ply2-name').innerHTML = "(" + commonMatchInfo.player2Hadicap + ") " + commonMatchInfo.player2Name;
+      }
 
+      // fill match scores
       document.getElementById('ply1-match-score').innerHTML = commonMatchInfo.player1MatchPoint;
       document.getElementById('ply2-match-score').innerHTML = commonMatchInfo.player2MatchPoint;
 
+      // fill race to value
+      if(commonMatchInfo.matchTotalFrames == '')
+      {
+        document.getElementById('total-score').innerHTML = "-";
+      }
+      else
+      {
+        document.getElementById('total-score').innerHTML = '(' + commonMatchInfo.matchTotalFrames + ')';
+      }
+
+      // fill frame scores
       document.getElementById('ply1-frame-score').innerHTML = commonMatchInfo.player1FramePoint ? commonMatchInfo.player1FramePoint : "";
       document.getElementById('ply2-frame-score').innerHTML = commonMatchInfo.player2FramePoint ? commonMatchInfo.player2FramePoint : "";
 
+      // show who is starting frame
       if (commonMatchInfo.player1Break)
       {
         document.getElementById('ply1-break').style.visibility = "visible";
@@ -74,27 +101,5 @@ xjs.ready().then(function() {
         document.getElementById('ply1-break').style.visibility = "hidden";
         document.getElementById('ply2-break').style.visibility = "visible";
       }
-
-      /**  Дмитрий~Константин~Игрок 3~0~0~0~1~6~0~0~0~0~1800~0~SNKR~9~0~1~18~1~2~
-        * tocken 1 - игрок 1
-        * tocken 2 - игрок 2
-        * tocken 3 - игнор
-        * tocken 4 - фора игрока 1 в снукере
-        * tocken 5 - фора игрока 2 в снукере
-        * tocken 6 - игнор
-        * tocken 7 - игнор
-        * tocken 8 - очки игрока 1 в матче
-        * tocken 9 - очки игрока 2 в матче
-        * tocken 10 - игнор
-        * tocken 11 - игнор
-        * tocken 12 - игнор
-        * tocken 13 - '0' - разбой игрока 1; '1800' - разбой игрока 2
-        * tocken 14 - игнор
-        * tocken 15 - [optional] - 'SNKR' - играют в снукер
-        * tocken 16 - [optional] - очки игрока 1 в снукере
-        * tocken 17 - [optional] - очки игрока 2 в снукере
-        * tocken 18 + [optional] - игнор
-
-      */
     })
 })
